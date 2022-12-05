c79834db45ecc08d6a5e34241a4c61dae477e025
Simon <simon.michael.te15d@gmail.com>
2022-12-05 12:06:07 +0100

    basic setup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/system';
+import { themeOptions } from './theme/material-theme';
+import { BrowserRouter } from 'react-router-dom';
+import SpotifyWebApi from 'spotify-web-api-node';
+import { redirectURL } from './config/config';
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+    redirectUri: redirectURL,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={themeOptions}>
+            <BrowserRouter>
+                <App spotifyApi={spotifyApi} />
+            </BrowserRouter>
+        </ThemeProvider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
