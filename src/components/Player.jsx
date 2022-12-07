26bf9657963d3dd6f9b27a25b58a40fcd765caee
Simon <simon.michael.te15d@gmail.com>
2022-12-07 16:02:21 +0100

    Added player controller

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Grid, Typography, Avatar } from '@mui/material';
+import { getAccessTokenFromStorage } from '../utils/getAccessTokenFromStorage';
+import PlayerControls from './PlayerControls';
 
 export default function Player({ spotifyApi }) {
+    const track = {
+        name: '',
+        album: {
+            images: [{ url: '' }],
+        },
+        artists: [{ name: '' }],
+    };
+
+    const [localPlayer, setPlayer] = useState(null);
+    const [is_paused, setPaused] = useState(false);
+    const [current_track, setTrack] = useState(track);
+    const [device, setDevice] = useState(null);
+    const [duration, setDuration] = useState(null);
+    const [progress, setProgress] = useState(null);
+
+    useEffect(() => {
+        const token = getAccessTokenFromStorage();
+        const script = document.createElement('script');
+        script.src = 'https://sdk.scdn.co/spotify-player.js';
+        script.async = true;
+        document.body.appendChild(script);
+
+        window.onSpotifyWebPlaybackSDKReady = () => {
+            const player = new window.Spotify.Player({
+                name: 'Web Playback SDK',
+                getOAuthToken: (cb) => {
+                    cb(token);
+                },
+                volume: 0.5,
+            });
+
+            player.addListener('ready', ({ device_id }) => {
+                console.log('Ready with Device ID', { device_id, player });
+                setDevice(device_id);
+                setPlayer(player);
+            });
+
+            player.addListener('player_state_changed', (state) => {
+                if (!state || !state.track_window?.current_track) {
+                    return;
+                }
+                console.log(state);
+                const duration_ms = state.track_window.current_track.duration_ms / 1000;
+                const position_ms = state.position / 1000;
+                setDuration(duration_ms);
+                setProgress(position_ms);
+                setTrack(state.track_window.current_track);
+                setPaused(state.paused);
+            });
+
+            setPlayer(player);
+            player.connect();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (!localPlayer) return;
+        localPlayer.connect();
+        return () => {
+            localPlayer.disconnect();
+        };
+    }, [localPlayer]);
+
+    useEffect(() => {
+        const transferMyPlayback = async () => {
+            if (device) {
+                await spotifyApi.transferMyPlayback([device], true);
+            }
+        };
+        const getDeviceFromApi = async () => {
+            await spotifyApi.getMyDevices();
+        };
+        getDeviceFromApi();
+        transferMyPlayback();
+    }, [device, spotifyApi]);
+
+    console.log('track');
+    console.log(current_track);
+
     return (
         <Box>
             <Grid
